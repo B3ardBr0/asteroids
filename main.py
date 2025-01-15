bf1c0dd83b7cbd6a5244c73e6e3df47b1839cceb
B3ardBr0 <wjmaritz@protonmail.com>
2025-01-15 05:35:13 +0300

    added pew-pew

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
@@ -13,8 +14,10 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
 
@@ -43,7 +46,7 @@ def main():
             obj.draw(screen)
 
         pygame.display.flip()
-w
+
         dt = clock.tick(60) / 1000
 
 
